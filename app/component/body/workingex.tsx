503694dd193e38f45a7dbe1746fad11f7bff7bf3
Anik Lal Dey <aniklal2020@gmail.com>
2024-12-26 08:34:57 +0600

    woking running phase 1

---
+++ app/component/body/workingex.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Workingex = () => {
+  return (
+    <div>
+      <div className='container mx-auto px-4'>
+           <div className='text-center'>
+            <p>Working Experience</p>
+           </div>
+      </div>
+    </div>
+  )
+}
+
+export default Workingex;
